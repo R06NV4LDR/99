126e09acbd313e4c753c1208c95bba69424e4b1b
Ronny A. Bruhin <ronny.bruhin+github@hotmail.com>
2025-06-24 15:56:03 +0200

    login and guestimport

--- public/createGuest.js
+++ public/createGuest.js
@@ -22,27 +22,36 @@ async function importGuests() {
     const g1 = row["Vorname 1"];
     const g2 = row["Vorname 2"];
 
-    const toBool = (v) => String(v).toLowerCase().trim() === "true"; 
+    const toBool = (v) => String(v).toLowerCase().trim() === "true";
     const invite06 = toBool(row["06.09.2025"]);
     const invite09 = toBool(row["09.09.2025"]);
 
     const guest = {
       g1_firstname: g1,
       g2_firstname: g2,
-      plus_one_allowed: g2 === "",
+      plus_one_allowed: g2.trim() === "",
       plus_one_name: "",
-      plus_one_foodinfos: "",
-      login: createLogin(g1, g2),
-      contact: "",
-      passw: "",
-      g1_allergies: "",
-      g2_allergies: "",
+      plus_one_attending: false,
+      g1_attending06: false,
+      g2_attending06: false,
+      plus_one_attending06: false,
+      g1_attending09: false,
+      g2_attending09: false,
+      plus_one_attending09: false,
       invite06,
       invite09,
       RSVP06: false,
       RSVP09: false,
-      createdAt: serverTimestamp()
+      contact: "",
+      login: createLogin(g1, g2),
+      passw: "",
+      confirmed: false,
+      g1_allergies: "",
+      g2_allergies: "",
+      plus_one_allergies: "",
+      last_updated: serverTimestamp()
     };
+
 
     try {
       await addDoc(collection(db, "Guests"), guest);
