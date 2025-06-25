620b4fa0db34538b007d9ec01f998ea1c41d50d3
Ronny A. Bruhin <ronny.bruhin+github@hotmail.com>
2025-06-25 12:27:48 +0200

    moved all files from public to root

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBIafeWH5E_kJM0swNvggIWMx5FhDRdyzI",
+  authDomain: "wedding99-80b1b.firebaseapp.com",
+  projectId: "wedding99-80b1b",
+  storageBucket: "wedding99-80b1b.firebasestorage.app",
+  messagingSenderId: "83161840112",
+  appId: "1:83161840112:web:543d845a7ce2ecb4e54535",
+  measurementId: "G-P5RR23JMYW"
+};
